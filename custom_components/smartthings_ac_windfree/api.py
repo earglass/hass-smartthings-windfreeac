036d4921eac5a65daa07f55ebb2b0118e6e20961
earglass <andrzej.pluszka@gmail.com>
2021-09-03 23:22:28 +0200

    add: re-built api.py + introduced climate.py del: media_player.py + soundbar related code del: (temp) remove README.md

---
+++ custom_components/smartthings_ac_windfree/api.py
@@ -0,0 +1,56 @@
+import requests
+import json
+
+
+class SmartthingsApi:
+    COMMAND_SWITCH_ON = "{'commands': [{'component': 'main','capability': 'switch','command': 'on'}]}"
+    COMMAND_SWITCH_OFF = "{'commands': [{'component': 'main','capability': 'switch','command': 'off'}]}"
+    COMMAND_REFRESH = "{'commands':[{'component': 'main','capability': 'refresh','command': 'refresh'}]}"
+    COMMAND_FAN_OSCILLATION_MODE = "{'commands':[{'component': 'main','capability': 'fanOscillationMode'," \
+                                   "'command': 'setFanOscillationMode'}]} "
+    COMMAND_OPTIONAL_MODE = "{'commands':[{'component': 'main','capability': 'custom.airConditionerOptionalMode'," \
+                            "'command': 'setAcOptionalMode'}]} "
+    COMMAND_FAN_MODE = "{'commands':[{'component': 'main','capability': 'fanMode'," \
+                       "'command': 'setFanMode'}]} "
+    COMMAND_TARGET_TEMPERATURE = "{'commands':[{'component': 'main','capability': 'thermostatCoolingSetpoint'," \
+                                 "'command': 'setCoolingSetpoint '}]} "
+
+    @staticmethod
+    def build_request_base(api_key: str, device_id: str, command: str):
+        request_headers = {"Authorization": "Bearer " + api_key}
+        url = "https://api.smartthings.com/v1/devices/" + device_id + command
+
+        return url, request_headers
+
+    @staticmethod
+    def send_command(api_key: str, device_id: str, command: str, arguments: json):
+        # TODO: handling wrong/missing command
+
+        command_body = json.loads(command)
+        command_body["commands"][0]["arguments"] = arguments
+        url, request_headers = SmartthingsApi.build_request_base(api_key, device_id, "/commands")
+        resp = requests.post(
+            url=url,
+            data=json.dumps(command_body),
+            headers=request_headers
+        )
+
+        if resp.status_code != 200:
+            # TODO: throw error
+            pass
+        if resp.json()['results'][0]['status'] != "ACCEPTED":
+            # TODO: throw error
+            pass
+
+    @staticmethod
+    def update_states(api_key, device_id):
+        SmartthingsApi.send_command(api_key, device_id, SmartthingsApi.COMMAND_REFRESH)
+
+        url, request_headers = SmartthingsApi.build_request_base(api_key, device_id, "/states")
+        resp = requests.get(
+            url=url,
+            headers=request_headers
+        )
+        # TODO: error handling
+
+        return resp.json()
---
+++ custom_components/smartthings_ac_windfree/climate.py
@@ -0,0 +1,235 @@
+"""Support for Adax wifi-enabled home heaters."""
+from __future__ import annotations
+
+import json
+import logging
+from typing import Any, List
+
+from homeassistant.components.climate import ClimateEntity
+from homeassistant.components.climate.const import (
+    SUPPORT_FAN_MODE,
+    SUPPORT_SWING_MODE,
+    SWING_OFF,
+    SWING_VERTICAL,
+    SWING_HORIZONTAL,
+    SWING_BOTH,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    ATTR_TEMPERATURE,
+    PRECISION_HALVES,
+    TEMP_CELSIUS,
+    CONF_NAME,
+    CONF_API_KEY,
+    CONF_DEVICE_ID,
+    STATE_OFF,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from api import SmartthingsApi
+
+_LOGGER = logging.getLogger(__name__)
+
+SWING_MODES = {
+    SWING_OFF: "fixed",
+    SWING_VERTICAL: "vertical",
+    SWING_HORIZONTAL: "horizontal",
+    SWING_BOTH: "all",
+}
+
+SUPPORTED_AC_OPTIONAL_MODES = [
+    "off",
+    "sleep",
+    "quiet",
+    "smart",
+    "speed",
+    "windFree",
+    "windFreeSleep",
+]
+
+
+async def async_setup_entry(
+        hass: HomeAssistant,
+        entry: ConfigEntry,
+        async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the Demo climate devices config entry."""
+    name = entry.data[CONF_NAME]
+    api_key = entry.data[CONF_API_KEY]
+    device_id = entry.data[CONF_DEVICE_ID]
+    async_add_entities([SamsungAc(api_key, device_id)])
+    # TODO: implement sensor for power consumption
+
+
+class SamsungAc(ClimateEntity):
+    """Representation of a heater."""
+
+    _attr_supported_features = SUPPORT_FAN_MODE | SUPPORT_SWING_MODE
+    _attr_target_temperature_step = PRECISION_HALVES
+    _attr_temperature_unit = TEMP_CELSIUS
+
+    # [x] "humidity",
+    # [x] "airConditionerMode",
+    # [ ] "doNotDisturb",
+    # [x] "fanOscillationMode",
+    # [x] "fanMode",
+    # [ ] "volume",
+    # [x] "coolingSetpoint",
+    # [x] "switch",
+    # [x] "temperature",
+    # [ ] "powerConsumption",
+    # [ ] "disabledCapabilities",
+    # [ ] "acOptionalMode",  # "[\"auto\",\"low\",\"medium\",\"high\",\"turbo\"]"
+    # [x] "supportedAcModes",  # "[\"aIComfort\",\"cool\",\"dry\",\"wind\",\"auto\",\"heat\"]"
+    # [x] "supportedAcFanModes",  # "[\"auto\",\"low\",\"medium\",\"high\",\"turbo\"]"
+    # [ ] "supportedFanOscillationModes", - currently returns null, but works ["fixed", "vertical", "horizontal", "all"]
+    # [ ] "supportedAcOptionalMode",  # "[\"off\",\"sleep\",\"quiet\",\"smart\",\"speed\",\"windFree\",
+    #                                       \"windFreeSleep\"]"
+
+    def __init__(self, api_key, device_id) -> None:
+        """Initialize the heater."""
+        # TODO: implement -> SmartthingsApi.get_name()
+        self._name = "Samsung AC"
+        self._api_key = api_key
+        self._device_id = device_id
+        self._state = STATE_OFF
+        self._states = {}
+
+    # @property
+    # def name(self) -> str:
+    #     """Return the name of the device, if any."""
+    #     return self._name
+
+    @property
+    def state(self) -> str:
+        """Return the current state."""
+        return self._states["switch"]
+
+    @property
+    def swing_mode(self) -> str:
+        """Return swing mode ie. fixed, vertical."""
+        return SWING_MODES[self._states["fanOscillationMode"]]
+
+    @property
+    def swing_modes(self) -> List[str]:
+        """Return the list of available swing modes.
+
+        Need to be a subset of HVAC_MODES.
+        """
+        # TODO: remove workaround once windfree & optional modes become separate property
+        SWING_MODES["windFree"] = "windFree"
+        return list(SWING_MODES.keys())
+
+    @property
+    def hvac_mode(self) -> str:
+        """Return hvac operation ie. heat, cool mode."""
+        return self._states["airConditionerMode"]
+
+    @property
+    def hvac_modes(self) -> List[str]:
+        """Return the list of available hvac operation modes.
+
+        Need to be a subset of HVAC_MODES.
+        """
+        # TODO: test if extra modes are passing
+        # self._states["supportedAcModes"].remove("aIComfort")
+        # self._states["supportedAcModes"].remove("wind")
+        return self._states["supportedAcModes"]
+
+    # @property
+    # def icon(self) -> str:
+    #     """Return nice icon for heater."""
+    #     # TODO: icons for each HVAC mode
+    #     # if self.hvac_mode == HVAC_MODE_HEAT:
+    #     #     return "mdi:fire"
+    #     # if self.hvac_mode == HVAC_MODE_OFF:
+    #     #     return "mdi:fire"
+    #     return "mdi:radiator-off"
+
+    @property
+    def current_temperature(self) -> float | None:
+        """Return the current temperature."""
+        return self._states["temperature"]
+
+    @property
+    def target_temperature(self) -> int | None:
+        """Return the temperature we try to reach."""
+        return self._states["coolingSetpoint"]
+
+    @property
+    def fan_mode(self):
+        """Return the fan setting."""
+        return self._states["fanMode"]
+
+    @property
+    def fan_modes(self):
+        """Return the list of available fan modes."""
+        # TODO: test if extra mode is passing
+        # self._states["supportedAcFanModes"].remove("turbo")
+        return self._states["supportedAcFanModes"]
+
+    @property
+    def current_humidity(self) -> int | None:
+        """Return the current humidity."""
+        return self._states["humidity"]
+
+    async def async_update(self) -> None:
+        """Get the latest data."""
+        data = SmartthingsApi.update_states(api_key=self._api_key, device_id=self._device_id)
+        for key, obj in data['main'].items():
+            self._states[key] = json.loads(obj['value'])
+
+    async def async_set_hvac_mode(self, hvac_mode: str) -> None:
+        """Set hvac mode."""
+        # TODO: implement hvac command
+        # SmartthingsApi.send_command()
+        self._attr_hvac_mode = hvac_mode
+        self.async_write_ha_state()
+
+    async def async_set_temperature(self, **kwargs: Any) -> None:
+        """Set new target temperature."""
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        if temperature is None:
+            return
+        self._attr_current_temperature = self._states["coolingSetpoint"]
+        SmartthingsApi.send_command(
+            api_key=self._api_key,
+            device_id=self._device_id,
+            command=SmartthingsApi.COMMAND_TARGET_TEMPERATURE,
+            arguments=[temperature]
+        )
+
+        self.async_write_ha_state()
+
+    async def async_set_swing_mode(self, swing_mode: str) -> None:
+        """Set new swing mode."""
+        self._attr_swing_mode = swing_mode
+
+        # TODO: remove workaround once windfree & optional modes become separate property
+        if swing_mode == "windFree":
+            SmartthingsApi.send_command(
+                api_key=self._api_key,
+                device_id=self._device_id,
+                command=SmartthingsApi.COMMAND_OPTIONAL_MODE,
+                arguments=[swing_mode]
+            )
+        else:
+            SmartthingsApi.send_command(
+                api_key=self._api_key,
+                device_id=self._device_id,
+                command=SmartthingsApi.COMMAND_FAN_OSCILLATION_MODE,
+                arguments=[swing_mode]
+            )
+        self.async_write_ha_state()
+
+    async def async_set_fan_mode(self, fan_mode: str) -> None:
+        """Set new fan mode."""
+        self._attr_fan_mode = fan_mode
+        SmartthingsApi.send_command(
+            api_key=self._api_key,
+            device_id=self._device_id,
+            command=SmartthingsApi.COMMAND_FAN_MODE,
+            arguments=[fan_mode]
+        )
+        self.async_write_ha_state()
